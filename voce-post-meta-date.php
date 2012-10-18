02a5db83575686ac3bb35e784a71eaacf75c61e3
Mark Parolisi <mark@voceconnect.com>
2012-10-18 10:56:20 -0400

    updating authors

--- voce-post-meta-date.php
+++ voce-post-meta-date.php
@@ -4,7 +4,7 @@
   Plugin URI: http://vocecommunications.com
   Description: Extends Voce Post Meta with a date picker field
   Version: 0.1
-  Author: Mark Parolisi
+  Author: markparolisi, voceplatforms
   Author URI: http://vocecommunications.com
   License: A "Slug" license name e.g. GPL2
  */
