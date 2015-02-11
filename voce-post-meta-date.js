f6f3c58f9f3608094a0ec4ee37e8ff786beaa75d
Curtis Loisel <curtis@voceconnect.com>
2015-02-11 09:09:17 -0500

    Setting up common callbacks to update the datetimefield

--- voce-post-meta-date.js
+++ voce-post-meta-date.js
@@ -1,18 +1,23 @@
 ;(function ( $, window, document, undefined ) {
 
+	var updateHidden = function( picker ) {
+		console.log(picker);
+	}
+
 	$(document).ready(function(){
+
+		var callbacks = {
+			close: function() {
+				var picker = this;
+				updateHidden(picker);
+			}
+		};
+
 		var $datepick = $( '.datepicker' ).pickadate();
-		var $datepicker = $datepick.pickadate('picker').on({
-			close: function() {
-				console.log(this.get('select'));
-			}
-		});
+		var $datepicker = $datepick.pickadate('picker').on(callbacks);
+
 		var $timepick = $( '.timepicker' ).pickatime();
-		var $timepicker = $timepick.pickatime('picker').on({
-			close: function() {
-				console.log(this.get('select'));
-			}
-		});
+		var $timepicker = $timepick.pickatime('picker').on(callbacks);
 
 	});
 
