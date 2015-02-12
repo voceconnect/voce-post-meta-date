99b0fbccc32132c3462939b2a83b5e88736604fd
Curtis Loisel <curtis@voceconnect.com>
2015-02-12 10:36:34 -0500

    Moving display function to class method

--- voce-post-meta-date.php
+++ voce-post-meta-date.php
@@ -136,47 +136,50 @@ class Voce_Post_Meta_Date {
 		$mapping['date'] = array(
 			'class' => 'Voce_Meta_Field',
 			'args' => array(
-				'display_callbacks' => array('voce_date_field_display'),
+				'display_callbacks' => array( array( __CLASS__, 'display_field' ) ),
 				'sanitize_callbacks' => array('voce_date_field_sanitize')
 			)
 		);
 		return $mapping;
 	}
+
+	public static function display_field( $field, $value, $post_id ) {
+		$defaults = array(
+			'min_date' => false,
+			'max_date' => false
+		);
+		$args = wp_parse_args( $field->args, $defaults );
+
+		$field_settings = array();
+
+		if ( !empty($args['min_date']) )
+			$field_settings['min'] = $args['min_date'] * 1000;
+
+		if ( !empty($args['max_date']) )
+			$field_settings['max'] = $args['max_date'] * 1000;
+
+		if ( $value ) {
+			$date_val = date('j F, Y', $value);
+			$time_val = date('g:i A', $value);
+		} else {
+			$date_val = 'Select Date';
+			$time_val = 'Select Time';
+		}
+		?>
+		<div>
+			<strong><?php voce_field_label_display( $field ); ?></strong>
+			<p><label>Date: </label><input type="text" class="datepicker" value="<?php echo esc_attr($date_val); ?>" /></p>
+			<p><label>Time: </label><input type="text" class="timepicker" value="<?php echo esc_attr($time_val); ?>" /></p>
+			<input type="hidden" class="vpm-datetime" data-field-settings="<?php echo esc_attr(json_encode($field_settings)); ?>" id="<?php echo $field->get_input_id(); ?>" name="<?php echo $field->get_name(); ?>" value="<?php echo esc_attr($value); ?>" />
+		</div>
+		<?php
+	}
+
 }
 
 Voce_Post_Meta_Date::initialize();
 
-function voce_date_field_display( $field, $value, $post_id ) {
-	$defaults = array(
-		'min_date' => false,
-		'max_date' => false
-	);
-	$args = wp_parse_args( $field->args, $defaults );
 
-	$field_settings = array();
-
-	if ( !empty($args['min_date']) )
-		$field_settings['min'] = $args['min_date'] * 1000;
-
-	if ( !empty($args['max_date']) )
-		$field_settings['max'] = $args['max_date'] * 1000;
-
-	if ( $value ) {
-		$date_val = date('j F, Y', $value);
-		$time_val = date('g:i A', $value);
-	} else {
-		$date_val = 'Select Date';
-		$time_val = 'Select Time';
-	}
-	?>
-	<div>
-		<strong><?php voce_field_label_display( $field ); ?></strong>
-		<p><label>Date: </label><input type="text" class="datepicker" value="<?php echo esc_attr($date_val); ?>" /></p>
-		<p><label>Time: </label><input type="text" class="timepicker" value="<?php echo esc_attr($time_val); ?>" /></p>
-		<input type="hidden" class="vpm-datetime" data-field-settings="<?php echo esc_attr(json_encode($field_settings)); ?>" id="<?php echo $field->get_input_id(); ?>" name="<?php echo $field->get_name(); ?>" value="<?php echo esc_attr($value); ?>" />
-	</div>
-	<?php
-}
 
 /**
  * Public callback function to display HTML meta form field
